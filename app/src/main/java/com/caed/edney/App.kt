448b8dc1b7650f3c9b98120a764c279320a6941e
Edney O. S. Filho <edney.osf@gmail.com>
2024-07-03 21:30:50 -0300

    Construindo tela de login

--- app/src/main/java/com/caed/edney/App.kt
+++ app/src/main/java/com/caed/edney/App.kt
@@ -1,7 +1,7 @@
 package com.caed.edney
 
 import android.app.Application
-import com.caed.edney.di.APP_MODULES
+import com.caed.edney.di.appModules
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
 
@@ -15,6 +15,6 @@ class App : Application() {
 
     private fun initKoin() = startKoin {
         androidContext(this@App)
-        modules(APP_MODULES)
+        modules(appModules)
     }
 }
